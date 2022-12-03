70094e5e0a8391d010b3adeb64bf3e315222a3f6
liuhui <liuhui@wangjiahuan.com.cn>
2022-12-03 10:06:29 +0800

    优化

--- config/database.php
+++ config/database.php
@@ -16,15 +16,15 @@ return [
             // 数据库类型
             'type'               =>  'mysql',
             // 服务器地址
-            'hostname'           =>  'mysql',
+            'hostname'           =>  '192.168.90.66',
             // 数据库名
-            'database'           =>  'greenwings',
+            'database'           =>  'gougucms',
             // 用户名
-            'username'           =>  'root',
+            'username'           =>  'dev',
             // 密码
-            'password'           =>  'EermfHtRhA',
+            'password'           =>  'devwjh',
             // 端口
-            'hostport'           =>  '3306',
+            'hostport'           =>  '30726',
             // 数据库表前缀
             'prefix'             =>  'cms_',
             // 数据库连接参数
