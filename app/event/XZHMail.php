4f3e2f8ad9129025dcd2423a228c1eeb7ff207f0
liuhui <liuhui@wangjiahuan.com.cn>
2022-12-05 13:27:05 +0800

    物流标签下载到本地

--- app/event/XZHMail.php
+++ app/event/XZHMail.php
@@ -10,23 +10,40 @@ use think\facade\Log;
 class XZHMail
 {
     public static function getLabels(){
-        $where = [['status','=',1],['shipment_id','<>',''],['express_no','=',''],['create_time','>=',time() - 7 * 86400]];
+        $where = [['status','=',1],['shipment_id','<>',''],['express_no|label_url','=',''],['create_time','>=',time() - 7 * 86400]];
         $list = RecycleOrder::where($where)->field(['id','shipment_id'])->limit(10)->order('create_time asc')->select();
         foreach ($list as $item){
             try {
                 $info = XZHMailApi::getLabels($item['shipment_id']);
                 if (!empty($info['shipment']['tracking_number'])){
+                    $labelUrl = self::downloadLabel($info['shipment']['parcels'][0]['label_url'],$info['shipment']['tracking_number']);
                     $updateData = [
                         'express_no'=>$info['shipment']['tracking_number'],
-                        'label_url'=> str_replace('http:','https:',$info['shipment']['parcels'][0]['label_url']),
+                        'label_url'=> $labelUrl,
                         'update_time'=>time()
                     ];
                     RecycleOrder::where('id',$item['id'])->update($updateData);
                     sleep(1);
                 }
             }catch (\Exception $e){
-                Log::error('获取订单的物流跟踪号错误',['error'=>$e->getMessage(),'params'=>$item]);
+                Log::error('获取订单的物流跟踪号错误'.json_encode(['error'=>$e->getMessage(),'params'=>$item]));
             }
         }
+    }
+
+    public static function downloadLabel($url,$fileName){
+        $fp_input = fopen($url, 'r');
+        if (!$fp_input){
+            Log::error('下载物流标签异常'.json_encode(['tracking_number'=>$fileName]));
+            return '';
+        }
+        $baseDir = app()->getRootPath() .'public';
+        $basePath = '/storage/labels/';
+        $file = $fileName.'.pdf';
+        if (!is_dir($baseDir.$basePath)) {
+            mkdir($baseDir.$basePath, 0750, true);
+        }
+        file_put_contents($baseDir.$basePath.$file, $fp_input);
+        return $basePath.$file;
     }
 }
